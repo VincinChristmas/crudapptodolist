ba828b973fe757f174e20028f73b7bedf635bd5e
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-11 17:19:57 +0800

    add new files today

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+//your body parser should always be put before your CRUD handlers.
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.listen(3002, function()  {
+console.log('I see you')
+})
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/tasktodo',(req, res) => {
+    console.log(req.body)
+})
+
+
+
+console.log("Never give up. May JavaScript be with you")
